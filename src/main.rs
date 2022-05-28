8391555b096c4f5b4ce767d24cce1573a941071a
Frikk Herding <frikksol@gmail.com>
2022-05-28 15:55:06 +0200

    ✨ Use Svelte
    Currently it is not possible to run from rust server with live reload

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use rocket_contrib::serve::StaticFiles;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use log::{info};
+
 #[macro_use] extern crate rocket;
+
+
+pub struct CORS;
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +33,9 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    info!("Starting server...");
+    info!(env!("CARGO_MANIFEST_DIR"));
+    rocket::ignite()
+    .attach(CORS)
+    .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/dist"))).launch();
 }
