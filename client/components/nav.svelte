997b4d36b0e6422fd387b2c811369b9e756c5a32
Frikk Herding <frikksol@gmail.com>
2022-05-30 22:51:43 +0200

    ✨ clicking outside of burger menu closes it

--- client/components/nav.svelte
+++ client/components/nav.svelte
@@ -12,14 +12,18 @@
 </script>
 
 {#if showMenu}
-<nav class="bg-green-300 flex flex-col h-screen md:w-[40vw] w-screen">
-    <IconButton src="menu.png" alt="hamburger" color="bg-yellow-300" hoverColor="bg-green-500" 
-        on:click={() => showMenu = !showMenu} />
+<div class="flex flex-row">
+    <button class="h-screen md:w-[60vw] w-0" on:click={() => showMenu = !showMenu} />
+    <nav class="bg-green-300 flex flex-col h-screen md:w-[40vw] w-screen flex-none">
+        <IconButton src="menu.png" alt="hamburger" color="bg-yellow-300" hoverColor="bg-green-500" 
+            on:click={() => showMenu = !showMenu} />
+    
+        <TextButton text="Home" on:click={() => onClickNavigate('/')}/>
+        <TextButton text="About me" on:click={() => onClickNavigate('/aboutMe')}/>
+        <TextButton text="Projects" on:click={() => onClickNavigate('/projects')}/>
+    </nav>
+</div>
 
-    <TextButton text="Home" on:click={() => onClickNavigate('/')}/>
-    <TextButton text="About me" on:click={() => onClickNavigate('/aboutMe')}/>
-    <TextButton text="Projects" on:click={() => onClickNavigate('/projects')}/>
-</nav>
 {:else}
 <nav class="bg-opacity-0">
     <IconButton src="menu.png" alt="hamburger" color="bg-green-500" hoverColor="bg-yellow-300" 
