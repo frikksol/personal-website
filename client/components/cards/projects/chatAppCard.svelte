485b819810143f3d90983f52e54e1ac3d408f30c
Frikk Herding <frikksol@gmail.com>
2022-06-21 21:19:03 +0200

    👽️ Update links and text for chat-app

--- client/components/cards/projects/chatAppCard.svelte
+++ client/components/cards/projects/chatAppCard.svelte
@@ -5,9 +5,10 @@
 
 <StandardCard headerColor="bg-orange-400" bodyColor="bg-white" title="Chat app">
   <p class="text-lg">
-    This is a realtime chat app which runs on a rust backend with rocket.
-    Currently the frontend is just static javascript and it looks kind of janky,
-    but the plan is to use a Svelte frontend for this when I get the time.
+    This is a realtime chat app which runs on a rust backend with rocket. The
+    fronend is written in typescript using SvelteKit and Tailwind CSS, but the
+    plan is to use a Svelte frontend for this when I get the time. The backend
+    is hosted on CloudRun and the frondend is hosted on Vercel.
     <br /><br />
     I wanted to start learning rust, since it seems like a really interesting language
     which can make fast and reliable code easier to write, and the fact that it runs
@@ -18,7 +19,7 @@
   <h1 class="pt-4 font-semibold">Technologies used:</h1>
   <div class="mt-2 flex flex-row flex-wrap space">
     <TechTag
-      href="https://chat-app-2x7ycovpza-ew.a.run.app/"
+      href="https://chat-app-flax-theta.vercel.app/"
       text="Visit Chat App"
       color="bg-orange-500"
     />
