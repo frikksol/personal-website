8ad131eb22182755b382948f0e967827be0d7a4a
Frikk Herding <frikksol@gmail.com>
2023-08-25 15:04:35 +0200

    💄 Update text

--- src/components/cards/about-me/unlocCard.svelte
+++ src/components/cards/about-me/unlocCard.svelte
@@ -8,18 +8,17 @@
   title="Integrations Lead at Unloc [2021-2023]"
 >
   <p class="text-lg">
-    In 2021 I started working at Unloc which is my current employer. We build a
-    platform for digital keys and the surrounding infrastructure. We connect
-    smart door locks with the management systems and build the infrastructure
-    for integrating the with service providers etc. I started as a Platform
-    Developer and after 8 months I changed position to be Integrations Lead;
-    managing the integrations we do with lock systems. In that position I do
-    backend and app development, communications with integrations partners and
-    support.
+    In 2021 I started working at Unloc, which builds a platform for digital keys
+    and the surrounding infrastructure. They connect smart door locks with the
+    management systems and build the infrastructure for integrating the with
+    service providers etc. I started as a Platform Developer and, after 8 months
+    I changed position to be Integrations Lead; managing the integrations with
+    with lock systems. In that position I did backend and app development,
+    communications with integrations partners and support.
     <br /><br />
     The backend is written in Typescript, runs on Google Cloud on a mix of CloudFunctions
     and CloudRun, while the app is currently a flutter app where a large portion
-    is native, written in Java and Swift.
+    is native, written in Java/Kotlin and Swift.
   </p>
   <h1 class="pt-4 font-semibold">Tags:</h1>
   <div class="space mt-2 flex flex-row flex-wrap">
